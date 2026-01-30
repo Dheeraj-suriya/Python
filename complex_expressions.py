9536172e941dd4d883d93a660aa7d0d044e11cc1
23BTCE116 <dheerajsuriya5@gmail.com>
2026-01-30 08:52:46 +0530

    Initialise commit

---
+++ complex_expressions.py
@@ -0,0 +1,8 @@
+a=int(input("Enter your age: "))
+if(a>=18):
+    print("You can watch ANIMAL movie")
+    print("Enjoy watching and have fun")
+else:
+    print("You are under 18")
+    print("Your are not alowed to watch")
+    print("Get the fuck out of here Boy")
